eb9cb9a4be15d957f324f1ba2df25de2fd3177da
raiguard <caleb.heuer@gmail.com>
2020-07-05 17:26:48 -0600

    Set up database

--- server/index.js
+++ server/index.js
@@ -1,9 +1,18 @@
+require("dotenv").config();
 const express = require("express");
+const massive = require("massive");
 
 const app = express();
 
 app.use(express.json());
 
-const SERVER_PORT = 4000;
+const { SERVER_PORT, CONNECTION_STRING } = process.env;
+
+massive({
+  connectionString: CONNECTION_STRING,
+  ssl: { rejectUnauthorized: false }
+})
+  .then(() => console.log("Database connection established"))
+  .catch((err) => console.log(err));
 
 app.listen(SERVER_PORT, () => console.log(`Server is running on port ${SERVER_PORT}`));
