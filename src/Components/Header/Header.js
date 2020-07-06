a567b8c406871b10673305dc4770ca0ba2f82a40
raiguard <caleb.heuer@gmail.com>
2020-07-06 15:37:09 -0600

    Convert header to function component

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,20 +1,18 @@
-import React, { Component } from "react";
+import React from "react";
 import { Link } from "react-router-dom";
 
 import "./Header.css";
 import logo from "../../assets/logo.png";
 
-export default class Header extends Component {
-  render = () => (
-    <header>
-      <img className="header-logo" src={logo} alt="Logo" />
-      <h1 className="header-title">SHELFIE</h1>
-      <Link className="header-nav-link" to="/">
-        Dashboard
-      </Link>
-      <Link className="header-nav-link" to="/add">
-        Add Inventory
-      </Link>
-    </header>
-  );
-}
+export default () => (
+  <header>
+    <img className="header-logo" src={logo} alt="Logo" />
+    <h1 className="header-title">SHELFIE</h1>
+    <Link className="header-nav-link" to="/">
+      Dashboard
+    </Link>
+    <Link className="header-nav-link" to="/add">
+      Add Inventory
+    </Link>
+  </header>
+);
